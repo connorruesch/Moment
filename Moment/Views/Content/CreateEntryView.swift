07ac12802d69dffcd945234012884ddc11759156
connorruesch <connorruesch@outlook.com>
2025-04-22 13:08:37 -0400

    add scroll view to entry modify and create views

--- Moment/Views/Content/CreateEntryView.swift
+++ Moment/Views/Content/CreateEntryView.swift
@@ -47,78 +47,79 @@ struct CreateEntryView: View {
     
     var body: some View {
         NavigationStack() {
-            VStack(alignment: .leading, spacing: 15) {
-                SectionTitle(systemName: "sparkles", title: "AMBIANCE")
-                
-                GlassView(cornerRadius: 15) {
-                    HStack {
-                        ForEach(Background.allCases, id: \.self) { choice in
-                            if(background == choice) {
-                                Circle()
-                                    .frame(width: 50, height: 50)
-                                    .foregroundStyle(choice.color().opacity(0.5))
-                                    .overlay {
-                                        Image(systemName: "checkmark")
-                                    }
-                            } else {
-                                Button {
-                                    background = choice
-                                } label: {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 15) {
+                    SectionTitle(systemName: "sparkles", title: "AMBIANCE")
+                    
+                    GlassView(cornerRadius: 15) {
+                        HStack {
+                            ForEach(Background.allCases, id: \.self) { choice in
+                                if(background == choice) {
                                     Circle()
                                         .frame(width: 50, height: 50)
-                                        .foregroundStyle(choice.color())
+                                        .foregroundStyle(choice.color().opacity(0.5))
+                                        .overlay {
+                                            Image(systemName: "checkmark")
+                                        }
+                                } else {
+                                    Button {
+                                        background = choice
+                                    } label: {
+                                        Circle()
+                                            .frame(width: 50, height: 50)
+                                            .foregroundStyle(choice.color())
+                                    }
                                 }
                             }
+                            
+                            Spacer()
                         }
-                        
-                        Spacer()
+                    }
+                    
+                    SectionTitle(systemName: "pencil", title: "ENTRY TITLE")
+                    
+                    GlassView(cornerRadius: 15) {
+                        TextField("Entry Title", text: $title)
+                    }
+                    
+                    SectionTitle(systemName: "text.document", title: "ENTRY TEXT")
+                    
+                    GlassView(cornerRadius: 15) {
+                        TextField("Entry Content", text: $content, axis: .vertical)
+                        .multilineTextAlignment(.leading)
+                        .frame(height: 200, alignment: .top)
+                    }
+                }
+                .padding()
+                .navigationTitle("Create Entry")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        SmallGlassButtonView(label: "Cancel") {
+                            dismiss()
+                        }
                     }
                 }
                 
-                SectionTitle(systemName: "pencil", title: "ENTRY TITLE")
-                
-                GlassView(cornerRadius: 15) {
-                    TextField("Entry Title", text: $title)
-                }
-                
-                SectionTitle(systemName: "text.document", title: "ENTRY TEXT")
-                
-                GlassView(cornerRadius: 15) {
-                    TextField("Entry Content", text: $content, axis: .vertical)
-                    .multilineTextAlignment(.leading)
-                    .frame(height: 200, alignment: .top)
-                }
-            }
-            .padding()
-            .navigationTitle("Create Entry")
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    SmallGlassButtonView(label: "Cancel") {
+                VStack {
+                    if(validateTitle() == false) {
+                        Text("Title is too many characters!")
+                            .foregroundStyle(.red)
+                    }
+                    
+                    LargeGlassButtonView(label: "Create") {
+                        let entry = Entry(title: title,
+                                          content: content,
+                                          background: background,
+                                          newsHeadline: newsHeadline,
+                                          newsImage: newsImage,
+                                          newsSource: newsSource)
+                        context.insert(entry)
                         dismiss()
                     }
+                    .disabled(!validateTitle())
                 }
+                .padding(.top, 20)
             }
-            
-            VStack {
-                if(validateTitle() == false) {
-                    Text("Title is too many characters!")
-                        .foregroundStyle(.red)
-                }
-                
-                LargeGlassButtonView(label: "Create") {
-                    let entry = Entry(title: title,
-                                      content: content,
-                                      background: background,
-                                      newsHeadline: newsHeadline,
-                                      newsImage: newsImage,
-                                      newsSource: newsSource)
-                    context.insert(entry)
-                    dismiss()
-                }
-                .disabled(!validateTitle())
-            }
-            .padding(.top, 20)
-            
             
             Spacer()
         }
--- Moment/Views/Content/ModifyEntryView.swift
+++ Moment/Views/Content/ModifyEntryView.swift
@@ -23,65 +23,65 @@ struct ModifyEntryView: View {
     
     var body: some View {
         NavigationStack(){
-            VStack(alignment: .leading, spacing: 15) {
-                SectionTitle(systemName: "sparkles", title: "AMBIANCE")
-                
-                GlassView(cornerRadius: 15) {
-                    HStack {
-                        ForEach(Background.allCases, id: \.self) { choice in
-                            if(entry.background == choice) {
-                                Circle()
-                                    .frame(width: 50, height: 50)
-                                    .foregroundStyle(choice.color().opacity(0.5))
-                                    .overlay {
-                                        Image(systemName: "checkmark")
-                                    }
-                            } else {
-                                Button {
-                                    entry.background = choice
-                                } label: {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 15) {
+                    SectionTitle(systemName: "sparkles", title: "AMBIANCE")
+                    
+                    GlassView(cornerRadius: 15) {
+                        HStack {
+                            ForEach(Background.allCases, id: \.self) { choice in
+                                if(entry.background == choice) {
                                     Circle()
                                         .frame(width: 50, height: 50)
-                                        .foregroundStyle(choice.color())
+                                        .foregroundStyle(choice.color().opacity(0.5))
+                                        .overlay {
+                                            Image(systemName: "checkmark")
+                                        }
+                                } else {
+                                    Button {
+                                        entry.background = choice
+                                    } label: {
+                                        Circle()
+                                            .frame(width: 50, height: 50)
+                                            .foregroundStyle(choice.color())
+                                    }
                                 }
                             }
+                            
+                            Spacer()
                         }
-                        
-                        Spacer()
+                    }
+                    
+                    SectionTitle(systemName: "pencil", title: "ENTRY TITLE")
+                    
+                    GlassView(cornerRadius: 15) {
+                        TextField("Entry Title", text: $entry.title)
+                    }
+                    
+                    SectionTitle(systemName: "text.document", title: "ENTRY TEXT")
+                    
+                    GlassView(cornerRadius: 15) {
+                        TextField("Entry Content", text: $entry.content, axis: .vertical)
+                            .multilineTextAlignment(.leading)
+                            .frame(height: 200, alignment: .top)
                     }
                 }
+                .padding()
+                .navigationTitle("Modify Entry")
                 
-                SectionTitle(systemName: "pencil", title: "ENTRY TITLE")
-                
-                GlassView(cornerRadius: 15) {
-                    TextField("Entry Title", text: $entry.title)
+                VStack {
+                    if(validateTitle() == false) {
+                        Text("Title is too many characters!")
+                            .foregroundStyle(.red)
+                    }
+                    
+                    LargeGlassButtonView(label: "Save") {
+                        dismiss()
+                    }
+                    .disabled(!validateTitle())
                 }
-                
-                SectionTitle(systemName: "text.document", title: "ENTRY TEXT")
-                
-                GlassView(cornerRadius: 15) {
-                    TextField("Entry Content", text: $entry.content, axis: .vertical)
-                    .multilineTextAlignment(.leading)
-                    .frame(height: 200, alignment: .top)
-                }
+                .padding(.top, 20)
             }
-            .padding()
-            .navigationTitle("Modify Entry")
-            
-            VStack {
-                if(validateTitle() == false) {
-                    Text("Title is too many characters!")
-                        .foregroundStyle(.red)
-                }
-                
-                LargeGlassButtonView(label: "Save") {
-                    dismiss()
-                }
-                .disabled(!validateTitle())
-            }
-            .padding(.top, 20)
-            
-            
             Spacer()
         }
     }
